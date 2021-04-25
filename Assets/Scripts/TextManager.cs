2df719fad8772067bdd54621c93055d8a3c8faae
glebDyakov <67467704+glebDyakov@users.noreply.github.com>
2021-04-25 20:42:32 +0300

    ui

---
+++ Assets/Scripts/TextManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextManager : MonoBehaviour
+{
+	public List<GameObject> texts;
+
+    void Start()
+    {
+	InvokeRepeating("ShowTexts", 1f, 1f);
+    }
+
+    public void ShowTexts()
+    {
+	if(texts[0].transform.childCount >= 1){
+		foreach(var text in texts){
+			text.transform.GetChild(0).gameObject.AddComponent<TextVisible>();
+		}
+		CancelInvoke();
+	}
+    }
+}
---
+++ Assets/Scripts/TextVisible.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class TextVisible : MonoBehaviour
+{
+    void Start()
+    {
+	GetComponent<TMP_SubMesh>().renderer.sortingOrder = 1;        
+    }
+
+    void Update()
+    {
+        
+    }
+}
